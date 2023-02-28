b5f72a5fbf97e0e5bf1c1d0df129cb84e25cdf40
Marwane Bidamou <devr.marwane@gmail.com>
2023-03-01 00:41:25 +0100

    First Commit

---
+++ src/Play.Common/MassTransit/Extensions.cs
@@ -0,0 +1,68 @@
+﻿using GreenPipes;
+using GreenPipes.Configurators;
+using MassTransit;
+using MassTransit.Definition;
+using MassTransit.ExtensionsDependencyInjectionIntegration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Play.Common.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Play.Common.MassTransit
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMassTrannsitWithRabbitMq(
+            this IServiceCollection services,
+            Action<IRetryConfigurator>? configureRetries = null
+            )
+        {
+            services.AddMassTransit(configure =>
+            {
+                //consumes registration
+                //consumers are the classes that are charges of consuming messages from the RabbitMQ messages
+                //we gonna register consumers by define the assembly that should have all the consumers already defined, and that's going to be the entry assemby for wichever microservice is invoking this class
+                configure.AddConsumers(Assembly.GetEntryAssembly());
+
+                configure.UsingPlayEconomyRabbitMQ(configureRetries);
+            });
+
+            services.AddMassTransitHostedService();
+
+
+            return services;
+        }
+
+        public static void UsingPlayEconomyRabbitMQ(
+            this IServiceCollectionBusConfigurator configure,
+            Action<IRetryConfigurator>? configureRetries = null
+            )
+        {
+            configure.UsingRabbitMq((context, configurator) =>
+            {
+                var configuration = context.GetService<IConfiguration>();
+                //service settings
+                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+                var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+                configurator.Host(rabbitMQSettings.Host);
+                configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+
+                if (configureRetries == null)
+                {
+                    configureRetries = (retryConfigurator) => {
+                        retryConfigurator.Interval(retryCount: 3, interval: TimeSpan.FromSeconds(5));
+                        //with that logic, anytime a message is not able to be consumed by a consumer, it'll be retried 3 times with a 5 seconds delay
+                    };                  
+                }
+
+                configurator.UseMessageRetry(configureRetries);
+            });
+        }
+    }
+}
---
+++ src/Play.Common/MongoDB/Extensions.cs
@@ -0,0 +1,56 @@
+﻿using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using Play.Common;
+using Microsoft.Extensions.DependencyInjection;
+using Play.Common.Settings;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+
+namespace Play.Common.MongoDB
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMongo(this IServiceCollection services)
+        {
+            //To Store guid type in mongo db as string
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+
+            //To Store DateTimeOffset type in mongo db as string
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+
+            //register mongo db service
+            services.AddSingleton(serviceProvider =>
+            {
+                var configuration = serviceProvider.GetService<IConfiguration>();
+                //service settings
+                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+                //mongo db settings
+                var mongoDbSettings = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                //prepare mongo client
+                var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
+                //get mongo database with the same name as the current service
+                return mongoClient.GetDatabase(serviceSettings.ServiceName);
+            });
+
+            return services;
+
+        }
+
+        public static IServiceCollection AddMongoRepository<T>(this IServiceCollection services, string collectionName) where T : IEntity
+        {
+            //we use service provider because we need to specify a parameter as an input to the repository
+            //(in MongoRepository we're receiving our collection name, so we cannot just expect to all parameters to be injected automatically by the service container for us)
+            services.AddSingleton<IRepository<T>>(serviceProvider =>
+            {
+                //before we can create an instance of the MongoRepository first, we need an instance of the IMongoDatabase
+                var database = serviceProvider.GetService<IMongoDatabase>();
+                return new MongoRepository<T>(database, collectionName);
+            });
+
+            return services;
+        }
+    }
+}
