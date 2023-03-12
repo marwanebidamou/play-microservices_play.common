7725d77d48df7f37c51e4c1ee50e0543165bf5b0
Marwane Bidamou <devr.marwane@gmail.com>
2023-03-12 15:52:00 +0100

    Added Cosmos DB support

--- src/Play.Common/Settings/MongoDbSettings.cs
+++ src/Play.Common/Settings/MongoDbSettings.cs
@@ -2,9 +2,20 @@
 {
     public class MongoDbSettings
     {
+        private string connectionString;
+
         public string? Host { get; init; }
         public int Port { get; init; }
 
-        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        public string ConnectionString
+        {
+            get
+            {
+                return string.IsNullOrWhiteSpace(connectionString)
+                    ? $"mongodb://{Host}:{Port}" : connectionString;
+            }
+            init { connectionString = value; }
+        }
+
     }
 }
